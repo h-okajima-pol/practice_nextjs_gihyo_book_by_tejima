e235e8f93e3b749e9a8ec3f1eb9bf13f7d023994
h-okajima-pol <h_okajima@pol-japan.co.jp>
2024-09-26 18:08:00 +0900

    finish setting of something that is needed

---
+++ src/themes/index.ts
@@ -0,0 +1,14 @@
+import colors from '@/themes/colors';
+import fontSizes from '@/themes/fontSizes';
+import letterSpacings from '@/themes/letterSpacings';
+import lettterSpacings from '@/themes/letterSpacings';
+import lineHeights from '@/themes/lineHeights';
+import space from '@/themes/spaces';
+
+export const theme = {
+    space,
+    // fontSizes,
+    letterSpacings,
+    lineHeights,
+    // colors
+}
---
+++ src/themes/spaces.ts
@@ -0,0 +1,9 @@
+const space = {
+    none: '0px',
+    small: '8px',
+    medium: '16px',
+    large: '32px',
+    extraLarge: '64px',
+} as const
+
+export default space;

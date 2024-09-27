1d3401ff5db15dd81c752085013314030f8e32c8
h-okajima-pol <h_okajima@pol-japan.co.jp>
2024-09-27 12:37:29 +0900

    divide typeguard func

--- src/services/auth/signin.ts
+++ src/services/auth/signin.ts
@@ -1,37 +1,10 @@
 import { ApiContext, User } from "@/types/data";
 import { fetcher } from "@/utils";
+import { checkUser } from "@/utils/type/guard";
 
 export type SigninParams = {
     username: string;
     password: string;
-}
-
-const checkUser = (obj:unknown): obj is User => {
-    if (typeof obj !== 'object' || obj === null) {
-        return false;
-    }
-
-    const hasRequiredKeys = (
-        'id' in obj &&
-        'username' in obj &&
-        'displayName' in obj &&
-        'email' in obj &&
-        'profileImageUrl' in obj &&
-        'description' in obj
-    );
-
-    if (!hasRequiredKeys) {
-        return false;
-    }
-
-    return (
-        typeof obj.id === 'number' &&
-        typeof obj.username === 'string' &&
-        typeof obj.displayName === 'string' &&
-        typeof obj.email === 'string' &&
-        typeof obj.profileImageUrl === 'string' &&
-        typeof obj.description === 'string'
-    )
 }
 
 const signin = async (
---
+++ src/utils/type/guard.ts
@@ -0,0 +1,29 @@
+import { User } from "@/types/data";
+
+export const checkUser = (obj:unknown): obj is User => {
+    if (typeof obj !== 'object' || obj === null) {
+        return false;
+    }
+
+    const hasRequiredKeys = (
+        'id' in obj &&
+        'username' in obj &&
+        'displayName' in obj &&
+        'email' in obj &&
+        'profileImageUrl' in obj &&
+        'description' in obj
+    );
+
+    if (!hasRequiredKeys) {
+        return false;
+    }
+
+    return (
+        typeof obj.id === 'number' &&
+        typeof obj.username === 'string' &&
+        typeof obj.displayName === 'string' &&
+        typeof obj.email === 'string' &&
+        typeof obj.profileImageUrl === 'string' &&
+        typeof obj.description === 'string'
+    )
+}

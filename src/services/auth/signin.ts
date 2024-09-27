b7e270e53cb5497cb36f10d12a8b8de6b87984c4
h-okajima-pol <h_okajima@pol-japan.co.jp>
2024-09-27 12:31:15 +0900

    add signin func

---
+++ src/services/auth/signin.ts
@@ -0,0 +1,60 @@
+import { ApiContext, User } from "@/types/data";
+import { fetcher } from "@/utils";
+
+export type SigninParams = {
+    username: string;
+    password: string;
+}
+
+const checkUser = (obj:unknown): obj is User => {
+    if (typeof obj !== 'object' || obj === null) {
+        return false;
+    }
+
+    const hasRequiredKeys = (
+        'id' in obj &&
+        'username' in obj &&
+        'displayName' in obj &&
+        'email' in obj &&
+        'profileImageUrl' in obj &&
+        'description' in obj
+    );
+
+    if (!hasRequiredKeys) {
+        return false;
+    }
+
+    return (
+        typeof obj.id === 'number' &&
+        typeof obj.username === 'string' &&
+        typeof obj.displayName === 'string' &&
+        typeof obj.email === 'string' &&
+        typeof obj.profileImageUrl === 'string' &&
+        typeof obj.description === 'string'
+    )
+}
+
+const signin = async (
+    context: ApiContext,
+    params: SigninParams
+): Promise<User> => {
+    const user = await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        },
+    )
+
+    if (!checkUser(user)) {
+        throw new Error('Invalid user data received');
+    }
+    
+    return user;
+}
+
+export default signin;

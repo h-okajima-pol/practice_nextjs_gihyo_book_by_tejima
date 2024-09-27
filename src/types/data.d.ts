4bfc02e27953ce18e1b7eb573df58aa47faf8b75
h-okajima-pol <h_okajima@pol-japan.co.jp>
2024-09-27 12:30:45 +0900

    add types

---
+++ src/types/data.d.ts
@@ -0,0 +1,29 @@
+// 商品カテゴリ
+export type Category = 'shoes' | 'clothes' | 'book';
+// 商品状態
+export type Condition = 'new' | 'used';
+// ユーザ
+export type User = {
+    id: number;
+    username: string;
+    displayName: string;
+    email: string;
+    profileImageUrl: string;
+    description: string;
+}
+// 商品
+export type Product = {
+    id: number;
+    category: Category;
+    title: string;
+    description: string;
+    imageUrl: string;
+    blurDataUrl: string;
+    price:number;
+    condition: Condition;
+    owner: User;
+}
+// API Context
+export type ApiContext = {
+    apiRootUrl: string;
+}
